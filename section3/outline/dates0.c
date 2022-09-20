dc15a111676962351873f8a3e3341aede52e3f3a
mollystoltz <112831798+mollystoltz@users.noreply.github.com>
2022-09-20 15:22:13 -0400

    Update dates0.c

--- section3/outline/dates0.c
+++ section3/outline/dates0.c
@@ -3,12 +3,22 @@
 #include <string.h>
 
 // Make Struct
+typedef struct
+{
+    int day;
+    int year;
+    int month;
+}
+date;
 
 int main(void)
 {
     // Initialize date
-
+    date today;
     // Add values
-
+    today.day = 20;
+    today.year = 2022;
+    today.month = "September";
     // Print date
+    printf("%s %i, %i\n", today.month, today.day, today.year);
 }
