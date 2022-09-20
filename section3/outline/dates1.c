da80c105f10b9764154cc1ba6517afa7d0ee70e5
mollystoltz <112831798+mollystoltz@users.noreply.github.com>
2022-09-20 15:33:19 -0400

    Update dates1.c

--- section3/outline/dates1.c
+++ section3/outline/dates1.c
@@ -11,17 +11,28 @@ typedef struct
 date;
 
 date get_date(void);
+void print_date(date d);
 
 int main(void)
 {
     // TODO
-
+    date today = get_date(); /prompts user for a date and assigns that to a variable 
     // Print date
     print_date(today);
 }
 
-
+//abstract date printing into a function
 void print_date(date d)
 {
     printf("%s %i, %i\n", d.month, d.day, d.year);
 }
+
+date get_date(void)
+{
+    date d;
+    d.day = get_int("Day: "\n);
+    d.month = get_string("Month: "\n);
+    d.year = get_int("Year: "\n);
+    return d;
+}
+
